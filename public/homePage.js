5f57ad65fc6e5b2ccb88811809fa2dcd9671b70a
“Sergey <savershinin23@gmail.com>
2025-08-06 01:45:21 +0300

    Доработка вывода ошибок по Итоговой работе

--- public/homePage.js
+++ public/homePage.js
@@ -30,34 +30,34 @@ updateRatesBoard();
 setInterval(updateRatesBoard,60000);
 
 moneyManager.addMoneyCallback = ({ currency, amount }) => {
-    ApiConnector.addMoney({ currency, amount }, ({ success, data }) => {
+    ApiConnector.addMoney({ currency, amount }, ({ success, error, data }) => {
         if(success){
             ProfileWidget.showProfile(data);
             moneyManager.setMessage(success, 'Баланс успешно пополнен!😊');
         } else {
-            moneyManager.setMessage(success, data || 'Ошибка! Что-то пошло не так.😥');
+            moneyManager.setMessage(success, error || 'Ошибка! Что-то пошло не так.😥');
         };
     });
 };
 
 moneyManager.conversionMoneyCallback = ({ fromCurrency, targetCurrency, fromAmount }) => {
-    ApiConnector.convertMoney({ fromCurrency, targetCurrency, fromAmount }, ({ success, data }) => {
+    ApiConnector.convertMoney({ fromCurrency, targetCurrency, fromAmount }, ({ success, error, data }) => {
         if(success){
             ProfileWidget.showProfile(data);
             moneyManager.setMessage(success, 'Конвертация выполнена успешно!😊');
         } else {
-            moneyManager.setMessage(success, data || 'Ошибка! Что-то пошло не так.😥');
+            moneyManager.setMessage(success, error || 'Ошибка! Что-то пошло не так.😥');
         }; 
     });
 };
 
 moneyManager.sendMoneyCallback = ({ to, currency, amount }) => {
-    ApiConnector.transferMoney({ to, currency, amount }, ({ success, data }) => {
+    ApiConnector.transferMoney({ to, currency, amount }, ({ success, error, data }) => {
         if(success){
             ProfileWidget.showProfile(data);
             moneyManager.setMessage(success, 'Перевод пользователю выполнен успешно!😊');
         } else {
-            moneyManager.setMessage(success, data || 'Ошибка! Что-то пошло не так.😥');
+            moneyManager.setMessage(success, error || 'Ошибка! Что-то пошло не так.😥');
         };
     });
 };
@@ -71,27 +71,27 @@ ApiConnector.getFavorites(({ success, data }) => {
 });
  
 favoritesWidget.addUserCallback = ({ id, name }) => {
-    ApiConnector.addUserToFavorites({id, name}, ({ success, data }) => {
+    ApiConnector.addUserToFavorites({id, name}, ({ success, error, data }) => {
         if(success){
             favoritesWidget.clearTable();
             favoritesWidget.fillTable(data);
             moneyManager.updateUsersList(data);
             favoritesWidget.setMessage(success, 'Пользователь добавлен в Избранное!😊');
         } else {
-            favoritesWidget.setMessage(success, data || 'Ошибка! Что-то пошло не так.😥');
+            favoritesWidget.setMessage(success, error || 'Ошибка! Что-то пошло не так.😥');
         };
     });
 };
 
-favoritesWidget.removeUserCallback = (id, callback) => {
-    ApiConnector.removeUserFromFavorites(id, ({ success, data }) => {
+favoritesWidget.removeUserCallback = (id) => {
+    ApiConnector.removeUserFromFavorites(id, ({ success, error, data }) => {
         if(success){
             favoritesWidget.clearTable();
             favoritesWidget.fillTable(data);
             moneyManager.updateUsersList(data);
             favoritesWidget.setMessage(success, 'Пользователь был удалён из Избранного!😊');
         } else {
-            favoritesWidget.setMessage(success, data || 'Ошибка! Что-то пошло не так.😥');
+            favoritesWidget.setMessage(success, error || 'Ошибка! Что-то пошло не так.😥');
         };
     });
 };
